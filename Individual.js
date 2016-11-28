3c5a38706e609132c8119ddbb971ba1ced0aeda3
snow-yan <1806678161@qq.com>
2016-11-29 00:34:36 +0800

    修改

--- Individual.js
+++ Individual.js
@@ -3,5 +3,20 @@ $(document).ready(function(){
 		var height=$(".produce1").position().top;
 		$("body,html").animate({scrollTop:height},500)
 	})
+	var pro=$(".produce1").position().top;
+	console.log(pro)
+	$(".nav1").on('click',function(){
+		$('body,html').animate({scrollTop:pro},600);
+		
+	})
+	var project=$(".study1").position().top;
+	$(".nav2").on('click',function(){
+		$('body,html').animate({scrollTop:project+90},600);	
+	})
+	
+	var proj=$(".wangye").position().top;
+	$(".nav3").on('click',function(){
+		$('body,html').animate({scrollTop:proj},600);	
+	})
 })
 
